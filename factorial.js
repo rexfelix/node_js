9d492b58ace9d80a7771f14b5232f18e7275ba6c
rexfelix <rexfelix.vonspica@gmail.com>
2020-10-05 02:31:21 +0900

    to-show.html, added

--- factorial.js
+++ factorial.js
@@ -5,5 +5,5 @@ function fact(n) {
 
 for (var i = 1; i < 10; i++) {
     // console.log(i+"!= "+fact(i));
-    document.write(i + "!= " + fact(i));
+    document.write(i + "!= " + fact(i)+"<br/>");
 }
